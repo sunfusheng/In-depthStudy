f5ddaab994ccfdecec6e239f1a2bf4421716f038
sfsheng0322 <sfsheng0322@gmail.com>
2015-12-24 10:29:18 +0800

    高质量编程学习

---
+++ app/src/main/java/com/sun/study/module/HighQualityCode/_4_String.java
@@ -0,0 +1,21 @@
+package com.sun.study.module.HighQualityCode;
+
+/**
+ * Created by sunfusheng on 15/12/24.
+ */
+public class _4_String {
+
+    /**
+     * 第四章 字符串
+     *
+     *  Although the world is full of suffering, it is full also of overcoming of it.
+     *  虽然世界充满了苦难，但总是能战胜的。－－Hellen Keller(海伦•凯勒 美国作家)
+     */
+
+    /**
+     * String、StringBuffer、StringBuilder
+     * String是不可变的，存在于字符串常量池
+     * StringBuffer是线程安全的
+     * StringBuilder不是线程安全的
+     */
+}
