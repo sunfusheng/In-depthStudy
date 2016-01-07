22f2e4cf84ce87b4482b489d8c2fba7b30d3b6f8
sfsheng0322 <sfsheng0322@gmail.com>
2016-01-07 20:10:36 +0800

    高质量编程学习

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -15,3 +15,8 @@
 #-keepclassmembers class fqcn.of.javascript.interface.for.webview {
 #   public *;
 #}
+
+-keep class * extends java.lang.annotation.Annotation
+-keepclasseswithmembernames class * {
+    native <methods>;
+}
--- app/src/main/java/com/sun/study/MainApplication.java
+++ app/src/main/java/com/sun/study/MainApplication.java
@@ -1,8 +1,11 @@
 package com.sun.study;
 
 import android.app.Application;
+import android.os.Environment;
 
+import com.alipay.euler.andfix.patch.PatchManager;
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.orhanobut.logger.Logger;
 import com.sun.study.framework.exception.CrashHandler;
 import com.sun.study.framework.proxy.ControlFactory;
 import com.sun.study.framework.sharedpreferences.FastJsonSerial;
@@ -15,6 +18,9 @@ import de.devland.esperandro.Esperandro;
 public class MainApplication extends Application {
 
     private static MainApplication instance;
+    private static PatchManager patchManager;
+
+    private static final String APATCH_PATH = "/out.apatch";
 
     @Override
     public void onCreate() {
@@ -29,10 +35,26 @@ public class MainApplication extends Application {
         Fresco.initialize(this);
 
         CrashHandler.getInstance().init(this);
+
+        patchManager = new PatchManager(this);
+        patchManager.init("1.1");
+        patchManager.loadPatch();
+
+        try {
+            String patchFileString = Environment.getExternalStorageDirectory().getAbsolutePath() + APATCH_PATH;
+            Logger.e("log-andfix", "patchFileString: "+patchFileString);
+            patchManager.addPatch(patchFileString);
+        } catch (Exception e) {
+            Logger.e("log-andfix", "Exception: "+e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     public static MainApplication getInstance() {
         return instance;
     }
 
+    public static PatchManager getPatchManager() {
+        return patchManager;
+    }
 }
