4750ec6c3c0dac6a1fd54905735eb7c2b03c68b7
sfsheng0322 <sfsheng0322@gmail.com>
2016-06-23 18:32:19 +0800

    add MarqueeView

--- app/src/main/java/com/sun/study/ui/activity/MarqueeViewActivity.java
+++ app/src/main/java/com/sun/study/ui/activity/MarqueeViewActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.widget.Toolbar;
 import com.sun.study.R;
 import com.sun.study.view.MarqueeView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
@@ -24,6 +27,8 @@ public class MarqueeViewActivity extends BaseActivity {
     MarqueeView marqueeView3;
     @Bind(R.id.marqueeView4)
     MarqueeView marqueeView4;
+    @Bind(R.id.marqueeView)
+    MarqueeView marqueeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,15 @@ public class MarqueeViewActivity extends BaseActivity {
         ButterKnife.bind(this);
 
         initToolBar(toolbar, true, "MarqueeView");
+
+        List<String> info = new ArrayList<>();
+        info.add("1. 大家好，我是孙福生。");
+        info.add("2. 欢迎大家关注我哦！");
+        info.add("3. GitHub帐号：sfsheng0322");
+        info.add("4. 新浪微博：孙福生微博");
+        info.add("5. 个人博客：sunfusheng.com");
+        info.add("6. 微信公众号：孙福生");
+        marqueeView.startWithList(info);
 
         marqueeView1.startWithText(getString(R.string.vs_res));
         marqueeView2.startWithText(getString(R.string.vs_res));
