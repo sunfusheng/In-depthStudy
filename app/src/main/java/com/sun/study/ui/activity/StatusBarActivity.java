7a8259339c1634ded2d2cdb718ec0112103b7b12
sfsheng0322 <sfsheng0322@gmail.com>
2016-08-14 16:43:37 +0800

    add StatusBar

--- app/src/main/java/com/sun/study/ui/activity/StatusBarActivity.java
+++ app/src/main/java/com/sun/study/ui/activity/StatusBarActivity.java
@@ -43,7 +43,6 @@ public class StatusBarActivity extends AppCompatActivity {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
             statusBarHeight = StatusBarUtil.getStatusBarHeight(this);
             Log.d("--->", "statusBarHeight: " + statusBarHeight);
         }
@@ -73,23 +72,6 @@ public class StatusBarActivity extends AppCompatActivity {
                 });
             }
         });
-
-
-
-
-
-
-//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            Window window = getWindow();
-//            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-//                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-//            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-//            window.setStatusBarColor(Color.TRANSPARENT);
-//            window.setNavigationBarColor(Color.TRANSPARENT);
-//        }
     }
 
 }
