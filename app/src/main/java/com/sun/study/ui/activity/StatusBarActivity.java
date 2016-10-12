be6e33b2e8cb01b3432814753ebaefc4b51149ff
sfsheng0322 <sfsheng0322@gmail.com>
2016-10-12 12:39:07 +0800

    添加状态栏工具

--- app/src/main/java/com/sun/study/ui/activity/StatusBarActivity.java
+++ app/src/main/java/com/sun/study/ui/activity/StatusBarActivity.java
@@ -1,20 +1,12 @@
 package com.sun.study.ui.activity;
 
-import android.app.Activity;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.ViewTreeObserver;
-import android.view.WindowManager;
 import android.widget.TextView;
 
+import com.android.util.statusbar.StatusBarCompat;
+import com.larswerkman.holocolorpicker.ColorPicker;
+import com.larswerkman.holocolorpicker.SVBar;
 import com.sun.study.R;
-import com.sun.study.util.ColorUtil;
-import com.sun.study.util.DisplayUtil;
-import com.sun.study.util.statusbar.StatusBarUtil;
-import com.sun.study.view.ObservableScrollView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -22,55 +14,29 @@ import butterknife.ButterKnife;
 /**
  * Created by sunfusheng on 16/8/11.
  */
-public class StatusBarActivity extends AppCompatActivity {
+public class StatusBarActivity extends BaseActivity implements ColorPicker.OnColorChangedListener {
 
-    @Bind(R.id.tv_title)
-    TextView tvTitle;
-    @Bind(R.id.sv_root)
-    ObservableScrollView svRoot;
-
-    private Activity mActivity;
-    private Context mContext;
-    private int statusBarHeight = 0;
-    private int titleBarHeight = 0;
+    @Bind(R.id.colorPicker)
+    ColorPicker colorPicker;
+    @Bind(R.id.svBar)
+    SVBar svBar;
+    @Bind(R.id.tv_color)
+    TextView tvColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mActivity = this;
-        mContext = this;
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            statusBarHeight = StatusBarUtil.getStatusBarHeight(this);
-            Log.d("--->", "statusBarHeight: " + statusBarHeight);
-        }
-
         setContentView(R.layout.activity_status_bar);
         ButterKnife.bind(this);
 
-        tvTitle.setPadding(0, (statusBarHeight + 10), 0, (statusBarHeight/2 - 10));
-        tvTitle.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                tvTitle.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-                titleBarHeight = tvTitle.getHeight();
-                Log.d("--->", "titleBarHeight: " + titleBarHeight);
-
-                svRoot.setOnScrollViewListener(new ObservableScrollView.OnScrollViewListener() {
-                    @Override
-                    public void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy) {
-                        float fraction = (y  * 1.0f) / (DisplayUtil.dip2px(mContext, 220)-titleBarHeight);
-                        if (fraction < 0) fraction = 0f;
-                        if (fraction > 1) fraction = 1f;
-
-                        Log.d("--->", "fraction: "+fraction+" y: "+y);
-                        tvTitle.setBackgroundColor(ColorUtil.getNewColorByStartEndColor(StatusBarActivity.this, fraction, R.color.transparent, R.color.red));
-                        tvTitle.setTextColor(ColorUtil.getNewColorByStartEndColor(StatusBarActivity.this, fraction, R.color.red, R.color.white));
-                    }
-                });
-            }
-        });
+        colorPicker.addSVBar(svBar);
+        colorPicker.setOldCenterColor(getResources().getColor(R.color.colorPrimary));
+        colorPicker.setOnColorChangedListener(this);
     }
 
+    @Override
+    public void onColorChanged(int color) {
+        tvColor.setText("" + color);
+        StatusBarCompat.setStatusBarColor(this, color);
+    }
 }
