5f1a24bcb2b7e7f0c7a9b6d7976a1fc7659f61cd
sfsheng0322 <sfsheng0322@gmail.com>
2016-11-10 16:45:22 +0800

    贝赛尔曲线

--- app/src/main/java/com/sun/study/view/QuadraticBezier.java
+++ app/src/main/java/com/sun/study/view/QuadraticBezier.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -30,6 +31,8 @@ public class QuadraticBezier extends View {
 
     private Path mPath = new Path();
 
+    private static final int REGION_WIDTH = 50;
+
     public QuadraticBezier(Context context) {
         super(context);
         init(context);
@@ -46,26 +49,31 @@ public class QuadraticBezier extends View {
     }
 
     private void init(Context context) {
+        // 起始点、终止点、控制点画笔
         mPaintPoint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintPoint.setStyle(Paint.Style.STROKE);
         mPaintPoint.setStrokeWidth(4);
         mPaintPoint.setColor(context.getResources().getColor(R.color.green));
 
+        // 贝赛尔曲线画笔
         mPaintBezier = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintBezier.setStyle(Paint.Style.STROKE);
         mPaintBezier.setStrokeWidth(8);
         mPaintBezier.setColor(context.getResources().getColor(R.color.colorPrimary));
 
+        // 辅助线画笔
         mPaintAuxiliary = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintAuxiliary.setStyle(Paint.Style.STROKE);
         mPaintAuxiliary.setStrokeWidth(4);
         mPaintAuxiliary.setColor(context.getResources().getColor(R.color.font_black_4));
 
+        // 辅助文字画笔
         mPaintAuxiliaryText = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintAuxiliaryText.setStyle(Paint.Style.STROKE);
         mPaintAuxiliaryText.setTextSize(DisplayUtil.dip2px(context, 12));
         mPaintAuxiliaryText.setColor(context.getResources().getColor(R.color.font_black_4));
 
+        // 起始点、终止点、控制点坐标
         mStartPoint = new PointEntity(GlobalParams.SCREEN_WIDTH / 4, GlobalParams.SCREEN_HEIGHT / 2);
         mEndPoint = new PointEntity(GlobalParams.SCREEN_WIDTH * 3 / 4, GlobalParams.SCREEN_HEIGHT / 2);
         mControlPoint = new PointEntity(GlobalParams.SCREEN_WIDTH / 4, GlobalParams.SCREEN_HEIGHT / 4);
@@ -92,15 +100,35 @@ public class QuadraticBezier extends View {
         canvas.drawPath(mPath, mPaintBezier);
     }
 
+    private boolean touchFlag = false;
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touchFlag = isTouchPointedRegion(mControlPoint, event.getX(), event.getY());
+                break;
             case MotionEvent.ACTION_MOVE:
-                mControlPoint.x = event.getX();
-                mControlPoint.y = event.getY();
-                invalidate();
+                if (touchFlag) {
+                    mControlPoint.x = event.getX();
+                    mControlPoint.y = event.getY();
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                touchFlag = false;
                 break;
         }
         return true;
+    }
+
+    // 是否触摸到相应的点
+    private boolean isTouchPointedRegion(PointEntity point, float touchX, float touchY) {
+        RectF rectF = new RectF();
+        rectF.set(point.x - REGION_WIDTH, point.y - REGION_WIDTH, point.x + REGION_WIDTH, point.y + REGION_WIDTH);
+        if (rectF.contains(touchX, touchY)) {
+            return true;
+        }
+        return false;
     }
 }
